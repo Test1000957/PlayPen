15741e5220f1dea37841d33c3458721f1d5dc098
BapnaS <BapnaS@MELYNP0553>
2019-11-20 11:30:59 +1100

    Changes

---
+++ WebDriverDemo/WebDriverDemo/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace WebDriverDemo
+{
+    [TestClass]
+    public class WebDriverDemo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WebDriver driver= new Crome
+        }
+    }
+}
---
+++ WebDriverDemo1/WebDriverDemo1/Demo.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverDemo1
+{
+    class Demo
+    {
+        IWebDriver driver;
+
+
+        [Test]
+        public void test()
+        {
+            String homeURL = "http://www.google.com";
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(homeURL);
+            WebDriverWait wait = new WebDriverWait(driver,System.TimeSpan.FromSeconds(15));
+
+
+            //wait.Until(driver =>driver.FindElement(By.XPath("//a[@href='/beta/login']")));
+
+
+            //IWebElement element =driver.FindElement(By.XPath("//a[@href='/beta/login']"));
+            //Assert.AreEqual("Sign In", element.GetAttribute("text"));
+        }
+
+    }
+
+}
